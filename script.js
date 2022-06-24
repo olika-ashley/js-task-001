e94350a426b2315742337c17030c02c89fbd2834
Ashley Olika <52158565+olika-ashley@users.noreply.github.com>
2022-06-24 21:45:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var firstName = "Ashley";
+var lastName = "Olika";
+var middleName = "Ifechukwudeh";
+var height = "177.8 cm";
+var country = "Nigeria"
+
+let name = lastName + " " + middleName + " " + firstName;
+
+document.write("Name:" +" " + name + "<br/>");
+document.write('Height:' + ' ' + height + '<br/>');
+document.write('Country:' + ' ' + country);
